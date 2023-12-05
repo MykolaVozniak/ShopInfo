e4a214c2606ee92d524569527f24098107616914
MVozniak <mikola.v.2016@gmail.com>
2023-12-05 15:21:18 +0200

    Recursive delete Category problem fix

--- DatabaseShop/dbo/Stored Procedures/Categories/sp_Category_Delete.sql
+++ DatabaseShop/dbo/Stored Procedures/Categories/sp_Category_Delete.sql
@@ -2,5 +2,6 @@
     @CategoryId INT
 AS
 BEGIN
+    UPDATE [dbo].[Categories] SET [MotherCategoryId] = NULL WHERE [MotherCategoryId] = @CategoryId;
     DELETE FROM Categories WHERE Id = @CategoryId;
 END;
