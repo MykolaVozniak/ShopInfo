c7f3f35baee0f739d1a65be973ea80463cc341a3
MVozniak <mikola.v.2016@gmail.com>
2023-12-05 18:22:52 +0200

    Recommit

--- DatabaseShop/dbo/Tables/Categories.sql
+++ DatabaseShop/dbo/Tables/Categories.sql
@@ -5,6 +5,6 @@
     [CategoryName]        NVARCHAR (25)  NOT NULL,
     [CategoryDescription] NVARCHAR (MAX) NULL,
     CONSTRAINT [PK_Categories] PRIMARY KEY CLUSTERED ([Id] ASC),
-    CONSTRAINT [FK_Categories_Categories1] FOREIGN KEY ([MotherCategoryId]) REFERENCES [dbo].[Categories] ([Id]) ON DELETE SET NULL,
+    CONSTRAINT [FK_Categories_Categories1] FOREIGN KEY ([MotherCategoryId]) REFERENCES [dbo].[Categories] ([Id]) ON DELETE NO ACTION,
     CONSTRAINT [FK_Categories_Shops] FOREIGN KEY ([ShopId]) REFERENCES [dbo].[Shops] ([Id]) ON DELETE SET NULL
 );
